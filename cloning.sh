0520112799204e4507183dcaf04acffc226c8798
odiador <arroa03@gmail.com>
2025-11-12 19:47:54 -0500

    Add initial project files including .gitignore, cloning script, and docker-compose configuration

---
+++ cloning.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git clone git@github.com:ProyectoFinal-Microservicios/apps.git
+git clone git@github.com:ProyectoFinal-Microservicios/architecture.git
+git clone git@github.com:ProyectoFinal-Microservicios/devOps.git
